19e4e7af7ae2fcd88b98f6604b057965c086c68b
Rachel Novoselac <r.novoselac@gmail.com>
2024-03-29 16:51:39 -0400

    refactored

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
-import express from 'express';
+// import express from 'express';
 import config from 'config';
+import axios from 'axios';
 
-const PORT = config.get('server').port;
-const ACCESS_TOKEN = config.get('ACCESS_TOKEN');
-const ACCOUNT_ID = config.get('ACCOUNT_ID');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Doodle Dash listening on port ${PORT}!`);
-
-});
-
-FB.api(
-    '/me',
-    'GET',
-    {"fields":"id,name,last_name","access_token":`${ACCESS_TOKEN}`,"X-App-Usage":null},
-    function(response) {
-        console.log(response);
-    }
-  );
-
-//trying child_process.exec from node to execute the curl
-
-// import exec from 'node:child_process';
-// const command = 'curl -i -X GET <https://graph.facebook.com/v19.0/me?fields=id%2Cname%2Clast_name&acces_token=>${ACCESS TOKEN}"';
-
-// exec(command, { encoding: 'utf-8' }, (error, stdout, stderr) => {
-//     if (error !== null) {
-//         console.log('Error', error, stderr);
-//         return;
-//     }
-
-//     const [responseMetadata, response] = stdout.split('\n\n');
-
-//     console.log('Metadata', JSON.parse(responseMetadata));
-//     console.log('Response', JSON.parse(response));
+// const PORT = config.get('server').port;
+// const app = express();
+// app.listen(PORT, () => {
+//   console.log(`Doodle Dash listening on port ${PORT}!`);
 // });
+
+const ACCESS_TOKEN = config.get('ACCESS_TOKEN');
+const url = 'https://graph.facebook.com/v19.0/me?fields=id%2Cname%2Clast_name&access_token=' + ACCESS_TOKEN;
+
+const fetchData = async () => {
+    try {
+        const response = await axios.get(url);
+        console.log(response.data); //do something
+        const percentageOfCallsUsed = JSON.parse(response.headers['x-app-usage']).call_count;
+        if (percentageOfCallsUsed > 80) {
+            setTimeout(fetchData, 90000);
+            console.log(percentageOfCallsUsed, '% of calls used!', 'calls reduced to every: 90 seconds');
+        } else {
+            setTimeout(fetchData, 2000);
+            console.log('calls every 2 seconds, ', 'used: ', percentageOfCallsUsed);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        setTimeout(fetchData, 5000);
+    }
+}
+fetchData();
