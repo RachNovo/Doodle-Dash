c534028b49fccf57bb3c6603be4102cc19dc964c
Rachel Novoselac <r.novoselac@gmail.com>
2024-04-01 13:14:38 -0400

    fixed spelling mistake

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const verifyConfigValues = () => {
         console.log('Looks like you have configured an ACCESS_TOKEN and ACCOUNT_ID :)');
         return true;
     } else {
-        console.log('Please retreive your ACCESS_TOKEN and ACCOUNT_ID from FaceBook\'s Graph API Tool and add them to a config file');
+        console.log('Please retrieve your ACCESS_TOKEN and ACCOUNT_ID from FaceBook\'s Graph API Tool and add them to a config file');
         return false;
     }
 
